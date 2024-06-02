5613d8d0f383a009118a691934376e3712e50ea7
anandu-ajesh <ananduajesh123@gmail.com>
2024-06-02 14:13:28 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
                 </motion.div>
                 <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 0.5 }} className="w-full lg:w-1/2">
                     <div className="flex justify-center lg:justify-start">
-                        <p className="my-10 max-w-xl py-6 text-2xl">{ABOUT_TEXT}</p>
+                        <p className="my-10 max-w-xl py-6">{ABOUT_TEXT}</p>
                     </div>
                 </motion.div>
             </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,8 +17,8 @@ const Hero = () => {
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
                         <motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-6 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Anandu Ajesh</motion.h1>
-                        <motion.span variants={container(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-3xl tracking-tight text-transparent">FrontEnd Developer</motion.span>
-                        <motion.p variants={container(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 text-2xl font-light tracking-tighter">{HERO_CONTENT}</motion.p>
+                        <motion.span variants={container(0.5)} initial="hidden" animate="visible" className=" text-3xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text  tracking-tight text-transparent">FrontEnd Developer</motion.span>
+                        <motion.p variants={container(1)} initial="hidden" animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
